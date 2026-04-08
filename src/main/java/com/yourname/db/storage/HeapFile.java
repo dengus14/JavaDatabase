e4631bd96fc3c329d23091687b30ee964c942bb4
dengus14 <ddgusev0318@gmail.com>
2026-04-08 16:58:05 -0500

    initialization of heapfile

--- src/main/java/com/yourname/db/storage/HeapFile.java
+++ src/main/java/com/yourname/db/storage/HeapFile.java
@@ -1,4 +1,10 @@
 package com.yourname.db.storage;
 
 public class HeapFile {
+
+    private DiskManager diskManager;
+    private int numPages;
+    public HeapFile(DiskManager diskManager) {
+        this.diskManager = diskManager;
+    }
 }
