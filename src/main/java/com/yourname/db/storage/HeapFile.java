cd1544a2e514a14c5e8a0a3f07530213b1008688
dengus14 <ddgusev0318@gmail.com>
2026-04-08 17:03:36 -0500

    constructor

--- src/main/java/com/yourname/db/storage/HeapFile.java
+++ src/main/java/com/yourname/db/storage/HeapFile.java
@@ -1,10 +1,15 @@
 package com.yourname.db.storage;
 
+import java.io.IOException;
+
 public class HeapFile {
 
     private DiskManager diskManager;
     private int numPages;
-    public HeapFile(DiskManager diskManager) {
-        this.diskManager = diskManager;
+
+
+    public HeapFile(String fileName) throws IOException {
+        this.diskManager = new DiskManager(fileName);
+        this.numPages = diskManager.getNumPages();
     }
 }
