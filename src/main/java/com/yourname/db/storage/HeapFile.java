23366abd66de2ce628711c9dadf0e77ac744bcd7
dengus14 <ddgusev0318@gmail.com>
2026-04-09 16:11:31 -0500

    insert method in HeapFile

--- src/main/java/com/yourname/db/storage/HeapFile.java
+++ src/main/java/com/yourname/db/storage/HeapFile.java
@@ -1,6 +1,7 @@
 package com.yourname.db.storage;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class HeapFile {
 
@@ -11,5 +12,22 @@ public class HeapFile {
     public HeapFile(String fileName) throws IOException {
         this.diskManager = new DiskManager(fileName);
         this.numPages = diskManager.getNumPages();
+    }
+
+    public RecordID insert(byte[] data)throws IOException {
+
+        for (int i = 0; i < numPages; i++) {
+            Page page = diskManager.readPage(i);
+            if (page.hasSpace(data.length)) {
+                RecordID id = page.insert(data).get();
+                diskManager.writePage(page);
+                return id;
+            }
+        }
+        Page allocatedPage = diskManager.allocatePage();
+        RecordID id = allocatedPage.insert(data).get();
+        diskManager.writePage(allocatedPage);
+        this.numPages++;
+        return id;
     }
 }
