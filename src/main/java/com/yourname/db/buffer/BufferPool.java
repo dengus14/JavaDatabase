8486a800d1a441bda170bcf059dbb23370f93a81
dengus14 <ddgusev0318@gmail.com>
2026-04-10 15:54:37 -0500

    constructor

--- src/main/java/com/yourname/db/buffer/BufferPool.java
+++ src/main/java/com/yourname/db/buffer/BufferPool.java
@@ -1,8 +1,21 @@
 package com.yourname.db.buffer;
 
+import com.yourname.db.storage.DiskManager;
 import com.yourname.db.storage.Page;
 
+import java.util.LinkedHashMap;
+
 public class BufferPool {
+
+    private int capacity;
+    private LinkedHashMap<Integer, Frame> map;
+    private DiskManager diskManager;
+
+    public BufferPool(int capacity, DiskManager diskManager) {
+        this.capacity = capacity;
+        this.diskManager = diskManager;
+        map = new LinkedHashMap<Integer, Frame>(capacity, 0.75f, true);
+    }
 
     private class Frame {
         private Page page;
