85cd46d9e9b9092b734c95a7b2978444818ebb98
dengus14 <ddgusev0318@gmail.com>
2026-04-12 14:16:01 -0500

    schema is done

--- src/main/java/com/yourname/db/record/Schema.java
+++ src/main/java/com/yourname/db/record/Schema.java
@@ -1,4 +1,47 @@
 package com.yourname.db.record;
 
+import java.sql.SQLException;
+import java.util.List;
+
 public class Schema {
+
+    private List<Column> columns;
+
+
+    public enum ColumnType {INT, VARCHAR, BOOLEAN}
+    public static class Column {
+        private String name;
+        private ColumnType type;
+        private int size;
+        public Column(String name, ColumnType type, int size) {
+            this.name = name;
+            this.type = type;
+            this.size = size;
+        }
+    }
+
+    public Schema(List<Column> columns) {
+        this.columns = columns;
+    }
+
+    public int getTotalSize() {
+        int totalSize = 0;
+        for (Column column : columns) {
+            totalSize += column.size;
+        }
+        return totalSize;
+    }
+
+    public int getOffset(String columnName) throws IllegalAccessException {
+        int runningByte = 0;
+        for (Column column : columns) {
+            if (column.name.equals(columnName)) {
+                return runningByte;
+            }
+            runningByte += column.size;
+        }
+
+        throw new IllegalArgumentException("Column name not found");
+    }
+
 }
