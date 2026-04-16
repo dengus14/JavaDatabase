0a1a2b40004921a0dfa80a6d6ab727fce9eccb66
dengus14 <ddgusev0318@gmail.com>
2026-04-15 21:42:19 -0500

    InternalNode is initialized

--- src/main/java/com/yourname/db/index/InternalNode.java
+++ src/main/java/com/yourname/db/index/InternalNode.java
@@ -1,12 +1,17 @@
 package com.yourname.db.index;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InternalNode extends BPlusTreeNode{
 
 
+    private List<BPlusTreeNode> children;
 
-
-
-
+    public InternalNode() {
+        super();
+        this.children = new ArrayList<>();
+    }
 
     @Override
     public boolean isLeaf() {
