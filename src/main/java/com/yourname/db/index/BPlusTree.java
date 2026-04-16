5320a0be3de3682759db660bda442d29b19c465c
dengus14 <ddgusev0318@gmail.com>
2026-04-15 22:01:18 -0500

    initial implementation of search, continue tmr

--- src/main/java/com/yourname/db/index/BPlusTree.java
+++ src/main/java/com/yourname/db/index/BPlusTree.java
@@ -1,4 +1,33 @@
 package com.yourname.db.index;
 
+import com.yourname.db.storage.RecordID;
+
 public class BPlusTree {
+
+    private BPlusTreeNode root;
+    private int order;
+
+    public BPlusTree(int order) {
+        this.order = order;
+        root = new LeafNode(null);
+    }
+
+    public RecordID search(int key){
+        BPlusTreeNode current = root;
+
+        if (current.isLeaf()){
+            LeafNode newCurr = (LeafNode) current;
+            for (int i = 0; i < current.keys.size(); i++){
+                if (i == key){
+                    return newCurr.getRecordIDs().get(i);
+                }
+            }
+        }
+
+        //continue implementing here
+
+
+
+        return null;
+    }
 }
--- src/main/java/com/yourname/db/index/LeafNode.java
+++ src/main/java/com/yourname/db/index/LeafNode.java
@@ -1,11 +1,13 @@
 package com.yourname.db.index;
 
 import com.yourname.db.storage.RecordID;
+import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class LeafNode extends BPlusTreeNode {
+    @Getter
     private List<RecordID> recordIDs;
     private LeafNode next;
 
