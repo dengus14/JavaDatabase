1499acb5d0f7d7c1f561e2c26493755f17b3f2d1
dengus14 <ddgusev0318@gmail.com>
2026-04-20 10:57:01 -0500

    createTable method completed

--- src/main/java/com/yourname/db/catalog/Catalog.java
+++ src/main/java/com/yourname/db/catalog/Catalog.java
@@ -3,16 +3,32 @@ package com.yourname.db.catalog;
 import com.yourname.db.record.Schema;
 import com.yourname.db.storage.HeapFile;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Catalog {
-    private Map<String, TableMetaData> schemas;
+    private Map<String, TableMetaData> tables;
 
 
     private class TableMetaData {
         String tableName;
         Schema schema;
         HeapFile heapFile;
+    }
+
+
+    public void createTable(String tableName, Schema schema, String filePath) throws IllegalStateException, IOException {
+        if (tables.containsKey(tableName)) {
+            throw new IllegalStateException("Table " + tableName + " already exists");
+        }
+
+        TableMetaData tableMetaData = new TableMetaData();
+        tableMetaData.tableName = tableName;
+        tableMetaData.schema = schema;
+        tableMetaData.heapFile = new HeapFile(filePath);
+
+        tables.put(tableName, tableMetaData);
+
     }
 }
