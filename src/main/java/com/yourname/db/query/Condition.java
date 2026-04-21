13df27e228af98e0bf669c65d83139b781ea1d00
dengus14 <ddgusev0318@gmail.com>
2026-04-20 22:04:07 -0500

    seqscan execute is completed

--- src/main/java/com/yourname/db/query/Condition.java
+++ src/main/java/com/yourname/db/query/Condition.java
@@ -3,9 +3,9 @@ package com.yourname.db.query;
 public class Condition {
 
     //this class represents "WHERE" clause for SQL statements
-    private String column;
-    private String value;
-    private String operator; // "=" "<" ">"
+    public String column;
+    public String value;
+    public String operator; // "=" "<" ">"
 
     public Condition(String column, String operator, String value) {
         this.column = column;
--- src/main/java/com/yourname/db/query/Executor.java
+++ src/main/java/com/yourname/db/query/Executor.java
@@ -1,7 +1,14 @@
 package com.yourname.db.query;
 
 import com.yourname.db.catalog.Catalog;
+import com.yourname.db.record.Schema;
+import com.yourname.db.storage.HeapFile;
+import com.yourname.db.storage.RecordID;
+import com.yourname.db.record.Record;
 
+import javax.management.Query;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Executor {
@@ -9,6 +16,49 @@ public class Executor {
     private Catalog catalog;
     public Executor(Catalog catalog) {
         this.catalog = catalog;
+    }
+
+
+    public List<Record> execute(QueryPlan queryPlan) {
+
+    }
+
+    public List<Record> executeSeqScan(QueryPlan queryPlan) throws IOException {
+        List<Record> records = new ArrayList<>();
+
+        HeapFile heapFile = catalog.getHeapFile(queryPlan.tableName);
+        Schema schema = catalog.getSchema(queryPlan.tableName);
+        List<RecordID> rids = heapFile.scan();
+        boolean hasCondition = queryPlan.condition != null;
+        Condition condition = queryPlan.condition;
+        Object conditionValue = null;
+
+        if (hasCondition) {
+            Schema.Column col = schema.getColumn(condition.column);
+            switch (col.type()) {
+                case INT:
+                    conditionValue = Integer.parseInt(condition.value);
+                    break;
+                case BOOLEAN:
+                    conditionValue = Boolean.parseBoolean(condition.value);
+                    break;
+                default:
+                    conditionValue = condition.value;
+            }
+        }
+
+        for (RecordID rid: rids) {
+            byte[] rawData = heapFile.get(rid);
+            Record newRecord = Record.deserialize(rawData, schema);
+            if (hasCondition) {
+                Object recordValue = newRecord.get(condition.column);
+                if (!recordValue.equals(conditionValue)) {
+                    continue;
+                }
+            }
+            records.add(newRecord);
+        }
+        return records;
     }
 
 
